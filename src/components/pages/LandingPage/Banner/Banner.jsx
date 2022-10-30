f2c52af03d984fc5b602fc91219b26157935d4f8
ayush-beniwal <103768983+ayush-beniwal@users.noreply.github.com>
2022-10-30 13:19:15 +0530

    landing-page-animation

--- src/components/pages/LandingPage/Banner/Banner.jsx
+++ src/components/pages/LandingPage/Banner/Banner.jsx
@@ -1,12 +1,25 @@
 /* eslint-disable react/react-in-jsx-scope */
+import { motion } from 'framer-motion';
 import Classes from './Banner.module.css';
 
 function Banner(props) {
   return (
     <div className={Classes.banner}>
-      <img src="/HERO-WO-LOGO-011.svg" className={Classes.image} />
+      <motion.img
+        initial={{ scale: 0.8, opacity: 0 }}
+        animate={{ scale: 1, opacity: 1 }}
+        transition={{ type: 'easein', duration: 5, delay: 0.5 }}
+        src="/HERO-WO-LOGO-011.svg"
+        className={Classes.image}
+      />
       <img src="/mobile-hero-section-02.svg" className={Classes.mobile_hero} />
-      <img src="/Group_7139.svg" className={Classes.logo_dates} />
+      <motion.img
+        initial={{ opacity: 0 }}
+        animate={{ opacity: 1 }}
+        transition={{ type: 'easein', duration: 2, delay: 3 }}
+        src="/Group_7139.svg"
+        className={Classes.logo_dates}
+      />
     </div>
   );
 }
