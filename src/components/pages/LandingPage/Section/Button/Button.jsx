64324b3c372526732b8885bb46ca4a51a060bd75
hustlernik <hustlernikxd@gmail.com>
2023-09-01 12:44:32 +0530

    src folder deleted

--- src/components/pages/LandingPage/Section/Button/Button.jsx
+++
@@ -1,7 +0,0 @@
-import Classes from './Button.module.css';
-
-function Button(props) {
-  return <button className={Classes.button + ' ' + Classes.glow_button}>{props.children}</button>;
-}
-
-export default Button;
